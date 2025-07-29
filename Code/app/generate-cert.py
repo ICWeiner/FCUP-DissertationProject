fddd325a227ad2e555b69a8f2beff4f86f7f8ce0
ICWeiner <diogoffn@gmail.com>
2025-07-29 19:24:10 +0100

    final commit ,fr this time

--- Code/app/generate-cert.py
+++ Code/app/generate-cert.py
@@ -1,8 +1,13 @@
 import trustme
 
 ca = trustme.CA()
-cert = ca.issue_cert("192.168.57.22")
 
-cert.private_key_pem.write_to_path("server.key")
-cert.cert_chain_pems[0].write_to_path("server.pem")
+fastapi_cert = ca.issue_cert("localhost", "192.168.57.164")
+fastapi_cert.private_key_pem.write_to_path("fastapi.key")
+fastapi_cert.cert_chain_pems[0].write_to_path("fastapi.pem")
+
+cert = ca.issue_cert("192.168.57.22")
+cert.private_key_pem.write_to_path("pve.key")
+cert.cert_chain_pems[0].write_to_path("pve.pem")
+
 ca.cert_pem.write_to_path("client.pem")
